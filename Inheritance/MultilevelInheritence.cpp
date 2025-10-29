2f6a7cd1cf13bac95def40a32389b0639611f349
Md. Rejwar Rahman Rifat <mdrejwarrahman1010@gmail.com>
2025-10-29 12:07:00 +0600

    Commit message for 29 October Tarikh

---
+++ Inheritance/MultilevelInheritence.cpp
@@ -0,0 +1,35 @@
+#include<iostream>
+using namespace std;
+
+class A {
+    public: 
+        void displayA() {
+            cout << " This is class A" <<endl;
+        }
+};
+
+
+class B: public A {
+    public: 
+        void displayB() {
+            cout << "THis is class B" << endl;
+        }
+};
+
+
+class C : public B {
+    public: 
+        void displayC(){
+            cout<< "This is class C" << endl;
+        }
+};
+
+int main() {
+    C obj;
+
+    obj.displayA();
+    obj.displayB();
+    obj.displayC();
+
+    return 0;
+}
