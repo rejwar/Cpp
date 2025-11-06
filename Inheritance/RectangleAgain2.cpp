b1f632a08ee05f2557dc80a6c35408ba6bd1370c
Md. Rejwar Rahman Rifat <mdrejwarrahman1010@gmail.com>
2025-11-06 12:07:00 +0600

    Commit message for 6 November Tarikh

---
+++ Inheritance/RectangleAgain2.cpp
@@ -0,0 +1,29 @@
+#include<iostream>
+using namespace std;
+
+class Shape {
+    public:
+        float width;
+        float length;
+
+};
+
+class Rectangle : public Shape {
+    public: 
+    float Area() {
+        return length * width;
+    }
+};
+
+int main() {
+    Rectangle r;
+
+    cout << " Enter width";
+    cin >> r.width;
+
+    cout << "Enter length";
+    cin >> r.length;
+
+    cout << " Area of Rectangle " << r.Area ()<< endl;
+    return 0;
+}
