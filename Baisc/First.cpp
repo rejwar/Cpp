c266a3f8e58a25a204379195d06f3f1878149091
Md. Rejwar Rahman Rifat <mdrejwarrahman1010@gmail.com>
2025-11-17 20:41:35 +0600

    New code

--- Baisc/First.cpp
+++ Baisc/First.cpp
@@ -2,3 +2,36 @@
 //
 
 //
+
+#include<iostream>
+using namespace std;
+
+class A {
+    public: 
+    void ShowA() {
+        cout << "I am A (Grandfather )\n";
+    }
+};
+
+class B: public A {
+    public: 
+    void ShowB() {
+        cout << "I am B {father }\n"; 
+    }
+};
+
+class C : public B {
+    public : 
+    void ShowC() {
+        cout <<" I am C (child )\b";
+    }
+};
+
+int main() {
+    C obj; 
+    obj.ShowA();
+    obj.ShowB();
+    obj.ShowC();
+
+    return 0;
+}
